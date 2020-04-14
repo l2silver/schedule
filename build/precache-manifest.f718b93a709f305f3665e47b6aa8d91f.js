228baa8b840a06c6f97f0f2b7e6044855993b0b2
Leigh Silverstein <l2silver@ryerson.ca>
2020-04-14 10:03:45 -0400

    firstBuild

---
+++ build/precache-manifest.f718b93a709f305f3665e47b6aa8d91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7650812de04d90e344a6080f7b22140f",
+    "url": "/l2silver/schedule/index.html"
+  },
+  {
+    "revision": "31d6dd209462acb8d4d3",
+    "url": "/l2silver/schedule/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "e94a40a803f25aaa7aea",
+    "url": "/l2silver/schedule/static/js/2.cf9c98ec.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/l2silver/schedule/static/js/2.cf9c98ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d6dd209462acb8d4d3",
+    "url": "/l2silver/schedule/static/js/main.8bcb26c0.chunk.js"
+  },
+  {
+    "revision": "fc3957c94c1443d81216",
+    "url": "/l2silver/schedule/static/js/runtime-main.453d931c.js"
+  }
+]);
